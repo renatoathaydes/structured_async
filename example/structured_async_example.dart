e7d8c5bf7957693ae8a2fe43d247f9e61f69cef6
Renato Athaydes <renato@athaydes.com>
2022-05-25 18:02:27 +0200

    Added group example.

--- example/structured_async_example.dart
+++ example/structured_async_example.dart
@@ -5,6 +5,19 @@ import 'package:structured_async/structured_async.dart';
 Future<int> theAnswer() async => 42;
 
 Future<void> main(List<String> args) async {
+  final cancel = args.contains('cancel');
+  final startTime = now();
+  print('Time  | Message\n'
+      '------+--------');
+  runZoned(() async {
+    await simpleExample(cancel);
+    await groupExample(cancel);
+  }, zoneSpecification: ZoneSpecification(print: (self, parent, zone, line) {
+    parent.print(zone, '${(now() - startTime).toString().padRight(5)} | $line');
+  }));
+}
+
+Future<void> simpleExample(bool cancel) async {
   // create a cancellable from any async function
   final cancellableAnswer = () async {
     // the function can only be cancelled at async points,
@@ -13,7 +26,7 @@ Future<void> main(List<String> args) async {
     return await theAnswer();
   }.cancellable();
 
-  if (args.contains('cancel')) {
+  if (cancel) {
     cancellableAnswer.cancel();
   }
 
@@ -35,7 +48,7 @@ Future<void> main(List<String> args) async {
     return 42;
   });
 
-  if (args.contains('cancel')) {
+  if (cancel) {
     cancelFriendlyTask.cancel();
   }
 
@@ -44,4 +57,62 @@ Future<void> main(List<String> args) async {
   } on InterruptedException {
     print('Still cannot compute the answer!');
   }
+}
+
+Future<void> groupExample(bool cancel) async {
+  final group = CancellableFuture.group([
+    () async => 1,
+    () async => 2,
+    () async => 3,
+  ], <int>[], intoList());
+
+  if (cancel) {
+    // group.cancel();
+  }
+
+  try {
+    print('Group basic result: ${await group}');
+  } on InterruptedException {
+    print('Group basic result was cancelled');
+  }
+
+  final group2 = [
+    () async => print('Started group 2, should print every second, up to 3s.'),
+    () => sleep(Duration(seconds: 1), () => print('1 second')),
+    () => sleep(Duration(seconds: 2), () => print('2 seconds')),
+    () => sleep(Duration(seconds: 3), () => print('3 seconds')),
+  ].cancellable(null, (void a, void b) => null);
+
+  scheduleMicrotask(() async {
+    if (cancel) {
+      print('Will cancel group 2 after 1 second, approximately...');
+      await sleep(Duration(milliseconds: 1100));
+      print('Cancelling!');
+      group2.cancel();
+    }
+  });
+
+  try {
+    await group2;
+    print('Done');
+  } on InterruptedException {
+    print('Group2 interrupted!');
+  }
+}
+
+int now() => DateTime.now().millisecondsSinceEpoch;
+
+/// Sleep for the given duration of time using small "ticks"
+/// to check if it's time to "wake up" yet.
+///
+/// This is done because it's not possible to cancel a submitted
+/// [Future.delayed] call. In the real world, actual async code
+/// would be used rather than [Future.delayed] anyway, so this
+/// should not be a problem in most applications.
+Future<void> sleep(Duration duration, [void Function()? function]) async {
+  final stopTime = now() + duration.inMilliseconds;
+  while (now() < stopTime) {
+    await Future.delayed(const Duration(milliseconds: 50));
+  }
+  function?.call();
 }
